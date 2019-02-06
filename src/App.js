f52153270f32e135a8b8975ca100bd797bbd9b57
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-06 11:47:07 +0100

    Header component created and connected to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+import Header from "./componentes/Header";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-
+            <div className="container">
+                <Header
+                    titulo = "Cotiza Criptomonedas al Instante"
+                />
             </div>
         );
     }
