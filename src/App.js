3478e818181763b9ca35be40387f65d837e71926
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-06 11:58:40 +0100

    Formulario component created and connected to App with some styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
 import Header from "./componentes/Header";
+import Formulario from "./componentes/Formulario";
 
 class App extends Component {
     render() {
         return (
             <div className="container">
                 <Header
-                    titulo = "Cotiza Criptomonedas al Instante"
+                    titulo="Cotiza Criptomonedas al Instante"
                 />
+                <div className="row justify-content-center">
+                    <div className="col-md-6 bg-light pb-4 contenido-principal">
+                        <Formulario/>
+                    </div>
+                </div>
             </div>
         );
     }
---
+++ src/componentes/Formulario.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class Formulario extends Component{
+    render() {
+        return (
+            <form>
+                <div className="form-group">
+                    <label>Moneda:</label>
+                    <select className="form-control">
+                        <option value="" disabled defaultValue>Elige tu moneda</option>
+                        <option value="EUR">Euros</option>
+                        <option value="GBD">Libras</option>
+                        <option value="USD">Dolar Estadounidense</option>
+                        <option value="MXN">Peso Mexicano</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label>Criptomoneda</label>
+                    <select className="form-control">
+
+                    </select>
+                </div>
+                <div className="form-group">
+                    <input type="submit" className="btn btn-primary" value="Cotizar"/>
+                </div>
+            </form>
+        )
+    }
+}
+
+export default Formulario;
