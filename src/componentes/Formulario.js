93daf40d2d6fda3e46d5cd8eb0cf810b8216682c
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-06 12:45:15 +0100

    Reading in console the form values

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -2,12 +2,30 @@ import React, {Component} from 'react';
 import OptionSelect from "./OptionSelect";
 
 class Formulario extends Component{
+
+    monedaRef = React.createRef();
+    cryptoRef = React.createRef();
+
+    cotizarMonedas = (e) => {
+        (e).preventDefault();
+
+        // Crear el objeto
+        const cotizacion = {
+            moneda: this.monedaRef.current.value,
+            crypto: this.cryptoRef.current.value
+        };
+
+        console.log(cotizacion);
+
+        // Enviar por props
+    };
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.cotizarMonedas}>
                 <div className="form-group">
                     <label>Moneda:</label>
-                    <select className="form-control">
+                    <select ref={this.monedaRef} className="form-control">
                         <option value="" disabled defaultValue>Elige tu moneda</option>
                         <option value="EUR">Euros</option>
                         <option value="GBD">Libras</option>
@@ -17,7 +35,7 @@ class Formulario extends Component{
                 </div>
                 <div className="form-group">
                     <label>Criptomoneda</label>
-                    <select className="form-control">
+                    <select ref={this.cryptoRef} className="form-control">
                         {Object.keys(this.props.monedas).map(key => (
                             <OptionSelect
                                 key={key}
